d70aba4897516acd788f070fa80139d5e808677c
dongck <525880573@qq.com>
2019-02-25 17:37:47 +0800

    搭建eureka 注册中心

--- productDemo/src/main/java/com/controller/IndexController.java
+++ productDemo/src/main/java/com/controller/IndexController.java
@@ -1,10 +1,16 @@
 package com.controller;
 
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.discovery.DiscoveryClient;
+
 @RestController
 public class IndexController {
+	
+	private final Logger logger = Logger.getLogger(getClass());
 	
 	@RequestMapping("/hello")
 	public String hello() {
